04c0a497a3ee93a500c1e427486a6d682c0e7e40
matthewjaroszek <matthewj369@icloud.com>
2026-04-07 22:53:44 -0400

    update

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
-import pandas as pd
-from dotenv import load_dotenv
 import os
 import sqlite3 as sql
-import psycopg2 as pgs
-import sys, argparse
 from flask import Flask, jsonify, request
+from pathlib import Path
 
-load_dotenv()
 sql_url = os.getenv('sql_url')
 db = 'recent_capitol_final.db'
 
