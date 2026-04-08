df339a30b0a3c134f4731bff1b35b201bb6a6478
matthewjaroszek <matthewj369@icloud.com>
2026-04-08 00:12:35 -0400

    update

--- api.py
+++ api.py
@@ -10,6 +10,12 @@ def help():
 def health():
     return jsonify({"status": "ok"})
 
+@app.route('/api/<country>/locations')
+def health(country):
+    x.execute(f'SELECT locattion_name FROM locations WHERE country = {country}')
+    return jsonify({country: x.fetchone()})
+
+"""
 @app.route('/api/weather/<city>')
 def get_weather(city):
     x.execute("SELECT * FROM rc WHERE city = ? LIMIT 1", (city,))
@@ -24,6 +30,7 @@ def get_all_weather():
     x.execute("SELECT country, city, condition, lat, lon FROM rc LIMIT 20")
     rows = x.fetchall()
     return jsonify([dict(zip([desc[0] for desc in x.description], row)) for row in rows])
+"""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
