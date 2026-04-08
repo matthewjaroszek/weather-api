bfc86c85f2485baf3af0cbc53c41446dee61aedd
matthewjaroszek <matthewj369@icloud.com>
2026-04-08 01:39:25 -0400

    update

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ app = Flask('APP')
 def help_api():
     routes = []
     for rule in app.url_map.iter_rules():
-        if str(rule) == "/api/sql/<path:cmd>": routes.append(str(rule) + "use %20 for space and %27 for \'")
+        if str(rule) == "/api/sql/<path:cmd>": routes.append(str(rule) + " - use %20 for space and %27 for quotes")
         else: routes.append(str(rule))
     return ret(routes)
 
@@ -16,36 +16,36 @@ def execute(cmd):
     x.execute(cmd)
     ret = x.fetchall()
     conn.close()
-    return jsonify(ret)
+    return ret(ret)
 
 @app.route('/api/health')
 def health():
-    return jsonify("status ok")
+    return "status ok"
 
 @app.route('/api/schema')
 def schema():
     x, conn = connect()
     ret = get_pragmas(x)
     conn.close()
-    return jsonify(ret)
+    return ret(ret)
 
 @app.route('/api/countries')
 def countries():
     x, conn = connect()
     x.execute(f'SELECT country FROM locations')
     rows = x.fetchall()
-    countries = [row[0] for row in rows]
+    ret = [row[0] for row in rows]
     conn.close()
-    return jsonify(countries)
+    return ret(ret)
 
 @app.route('/api/<country>/locations')
 def locations(country):
     x, conn = connect()
     x.execute(f'SELECT location_name FROM locations WHERE country = ?', (country,))
     rows = x.fetchall()
-    locations = [row[0] for row in rows]
+    ret = [row[0] for row in rows]
     conn.close()
-    return jsonify(locations)
+    return ret(ret)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug = False)
